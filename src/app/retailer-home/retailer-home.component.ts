8f2ac33185e466d3d5ef9810b656093afb62e3d6
Vidit Singhal <vs7342@g.rit.edu>
2018-06-17 16:36:53 -0400

    Adding navigation bar UI and basic route linking. Hiding links based on UserType still needs to be implemented.

--- src/app/retailer-home/retailer-home.component.ts
+++ src/app/retailer-home/retailer-home.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { PublicService } from '../services/public.service';
+import { ToasterComponent } from '../shared/toaster.component';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-retailer-home',
   templateUrl: './retailer-home.component.html',
-  styleUrls: ['./retailer-home.component.css']
+  styleUrls: ['./retailer-home.component.css'],
+  providers: [PublicService]
 })
 export class RetailerHomeComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(ToasterComponent) toastr: ToasterComponent;
+  retailer_url_part: string;
+  retailer_website_name: string;
+  retailer_template_id: number;
+
+  constructor(private publicService: PublicService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.fetchRetailerDetails();
+  }
+
+  fetchRetailerDetails() {
+    // First fetch selected retailer (using the url_part parameter in activated route)
+    this.retailer_url_part = this.route.snapshot.params['url_part'];
+    this.publicService.getSingleRetailer(this.retailer_url_part).subscribe(data => {
+      if (data['data']) {
+        // Url entered was correct
+        this.retailer_website_name = data['data']['Website_Title'];
+        this.retailer_template_id = data['data']['Client_Template_id'];
+      } else {
+        // Url part entered was incorrect / not found in db - Navigate to app component
+        this.router.navigate(['']);
+      }
+    }, error => {
+      this.toastr.showError(error);
+    });
   }
 
 }
