3ad904a91fbe1651cfdbc5ebca09414805f111e6
Vidit Singhal <vs7342@g.rit.edu>
2018-06-22 00:55:44 -0400

    Adding password update modal + logic. Modal structure / css were taken from connect-client repo.

--- src/app/retailer-home/retailer-home.component.ts
+++ src/app/retailer-home/retailer-home.component.ts
@@ -3,12 +3,14 @@ import { PublicService } from '../services/public.service';
 import { ToasterComponent } from '../shared/toaster.component';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ConstantsService } from '../services/constants.service';
+import { User } from '../models/User.model';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-retailer-home',
   templateUrl: './retailer-home.component.html',
   styleUrls: ['./retailer-home.component.css'],
-  providers: [PublicService]
+  providers: [PublicService, UserService]
 })
 export class RetailerHomeComponent implements OnInit {
 
@@ -19,7 +21,18 @@ export class RetailerHomeComponent implements OnInit {
   is_user_logged_in: boolean;
   user_type_id: number;
 
-  constructor(private publicService: PublicService, private route: ActivatedRoute, private router: Router) { }
+  // Password modal related attributes
+  password_modal_displayed = false;
+  current_password = '';
+  new_password = '';
+  re_password = '';
+
+  constructor(
+    private publicService: PublicService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
     this.fetchRetailerDetails();
@@ -66,6 +79,50 @@ export class RetailerHomeComponent implements OnInit {
 
     // Navigate to Login Component
     this.router.navigate(['./login'], { relativeTo: this.route });
+  }
+
+  showPasswordModal() {
+    this.password_modal_displayed = true;
+  }
+
+  hidePasswordModal() {
+    this.password_modal_displayed = false;
+  }
+
+  updatePassword() {
+    // Check if all the fields are filled
+    if (
+      this.current_password.length > 0 &&
+      this.new_password.length > 0 &&
+      this.re_password.length > 0) {
+
+      // Now check if the new password and re-enter password fields match or not
+      if (this.new_password === this.re_password) {
+
+        // Everything seems to be fine.. Now fetch user id and make the update password call
+        const logged_in_user = ConstantsService.getLoggedInUser() as User;
+        if (logged_in_user) {
+          this.userService.updatePassword(logged_in_user.id, this.current_password, this.new_password).subscribe(data => {
+            // Show success message
+            this.toastr.showSuccess(data['message']);
+
+            // Close the modal and reset the fields
+            this.password_modal_displayed = false;
+            this.current_password = '';
+            this.new_password = '';
+            this.re_password = '';
+          }, error => {
+            this.toastr.showError(error);
+          });
+        }
+
+      } else {
+        this.toastr.showWarning('Passwords don\'t match');
+      }
+
+    } else {
+      this.toastr.showWarning('Kindly fill all the fields');
+    }
   }
 
 }
