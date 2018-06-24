4f611844c0dee0e5716234627ace8c888a8b38bd
Vidit Singhal <vs7342@g.rit.edu>
2018-06-23 22:43:34 -0400

    Adding home page (product list) and single product page (UI + Logic) for public and logged in customers.

--- src/app/retailer-home/product/products.component.ts
+++ src/app/retailer-home/product/products.component.ts
@@ -1,15 +1,120 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Product } from '../../models/Product.model';
+import { ConstantsService } from '../../services/constants.service';
+import { PublicService } from '../../services/public.service';
+import { ProductService } from '../../services/product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToasterComponent } from '../../shared/toaster.component';
+import { Cart } from '../../models/Cart.model';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
+  providers: [PublicService, ProductService, CartService]
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  @ViewChild(ToasterComponent) toastr: ToasterComponent;
+  products: Product[] = [];
+  carts: Cart[] = [];
+  url_part = '';
+  sub: any;
+  user_id = 0;
+
+  constructor(
+    private publicService: PublicService,
+    private productService: ProductService,
+    private cartService: CartService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+
+    // Check if user is logged in or not
+    const id = ConstantsService.getLoggedInUserId();
+    if (id) {
+      // User is logged in.. We can call the normal get products call
+      this.user_id = id;
+      this.loadProducts(id);
+    } else {
+      // Since user is not logged in, we would be needing url part to fetch product list
+      this.user_id = 0;
+      // Fetch url part from parent route params
+      this.sub = this.route.parent.params.subscribe(params => {
+        this.url_part = params['url_part'];
+
+        // If something is wrong with the url_part, navigate to AppComponent
+        if (!this.url_part) {
+          this.router.navigate(['']);
+        } else {
+          this.loadProducts(0);
+        }
+      });
+    }
+  }
+
+  loadProducts(user_id) {
+    if (user_id === 0) {
+      // We need to call public endpoint to fetch products
+      this.publicService.getAllProducts(this.url_part).subscribe(data => {
+        this.products = data['data'];
+      }, error => {
+        this.toastr.showError(error);
+      });
+    } else {
+      // We now fetch all products in user's cart
+      // Cart details had to be fetched before otherwise isProductInCart() would always false
+      this.cartService.getCartForUser(this.user_id).subscribe(cart_data => {
+          this.carts = cart_data['data'];
+          // We need to call normal get products endpoint
+          this.productService.getAllProducts().subscribe(data => {
+            this.products = data['data'];
+          }, error => {
+            this.toastr.showError(error);
+          });
+        }, error => {
+          this.toastr.showError(error);
+        });
+    }
+  }
+
+  goToProduct(product_id: number, className: string) {
+    // Check if the event click is coming from 'btn'.
+    // If the event is coming from button click (Add to Cart) then we don't need to navigate to single product page
+    if (className !== 'btn btn-primary') {
+      this.router.navigate(['' + product_id], {relativeTo: this.route});
+    }
+  }
+
+  isProductInCart(product_id: number) {
+    // Loop through each cart instance and check if the product is already added in the cart
+    for (let i = 0; i < this.carts.length; i++) {
+      if (this.carts[i].Product_id === product_id) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  addToCart(product_id: number, className: string) {
+    // This will add the specified product in the cart with quantity 1
+    const cart = new Cart(-1, this.user_id, product_id, 1);
+    this.cartService.addProductToCart(cart).subscribe(data => {
+      this.toastr.showSuccess('Product added to cart successfully (Qty = 1)');
+      this.carts.push(cart);
+    }, error => {
+      this.toastr.showError(error);
+    });
+  }
+
+  ngOnDestroy() {
+    // sub will only be initialized when url_part is fetched i.e. when user is not logged in
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 
 }
--- src/app/retailer-home/product/single-product.component.ts
+++ src/app/retailer-home/product/single-product.component.ts
@@ -1,15 +1,179 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ToasterComponent } from '../../shared/toaster.component';
+import { Product } from '../../models/Product.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { ConstantsService } from '../../services/constants.service';
+import { PublicService } from '../../services/public.service';
+import { Cart } from '../../models/Cart.model';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-single-product',
   templateUrl: './single-product.component.html',
-  styleUrls: ['./single-product.component.css']
+  styleUrls: ['./single-product.component.css'],
+  providers: [ProductService, PublicService, CartService]
 })
-export class SingleProductComponent implements OnInit {
+export class SingleProductComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  @ViewChild(ToasterComponent) toastr: ToasterComponent;
+  product: Product = new Product(0, '', '', '', 0, '', 0, 0);
+  sub: any;
+  user_id = 0;
+  is_alert_active = false;
+  product_cart = new Cart(0, 0, 0, 1);
+
+  constructor(
+    private productService: ProductService,
+    private publicService: PublicService,
+    private cartService: CartService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    // Fetch product id
+    const product_id = this.route.snapshot.params['product_id'];
+
+    // Proceed only if product id is provided
+    if (product_id) {
+      this.product.id = product_id;
+
+      // Fetch user id if any - based on this we will decide whether to call public or customer endpoint
+      const user_id = ConstantsService.getLoggedInUserId();
+      if (user_id) {
+        // Customer endpoint
+        this.loadProductForCustomer();
+        this.user_id = user_id;
+        this.product_cart.User_id = user_id;
+      } else {
+        // Public endpoint
+        // Fetch url part from parent route params
+        this.sub = this.route.parent.params.subscribe(params => {
+          const url_part = params['url_part'];
+
+          // If something is wrong with the url_part, navigate to AppComponent
+          if (!url_part) {
+            this.router.navigate(['']);
+          } else {
+            this.loadProductForPublic(url_part);
+          }
+        });
+      }
+    }
+  }
+
+  loadProductForPublic(url_part: string) {
+    this.publicService.getSingleProducts(url_part, this.product.id).subscribe(data => {
+      // Need to check if product was found or not..
+      // If not, that means someone was trying to access product by entering random ID
+      if (data['data']) {
+        this.product = data['data'];
+      } else {
+        // Means product not found.. Navigate to product list
+        this.router.navigate(['../'], {relativeTo: this.route});
+      }
+    }, error => {
+      this.toastr.showError(error);
+    });
+  }
+
+  loadProductForCustomer() {
+    this.productService.getSingleProduct(this.product.id).subscribe(data => {
+
+      // Need to check if product was found or not..
+      // If not, that means someone was trying to access product by entering random ID
+      if (!data['data']) {
+        // Means product not found.. Navigate to product list
+        this.router.navigate(['../'], {relativeTo: this.route});
+        return;
+      }
+
+      // Initialize the product attribute
+      this.product = data['data'];
+      this.product_cart.Product_id = this.product.id;
+
+      // Get all cart instances for the user and extract single instance for the selected product if any
+      this.cartService.getCartForUser(this.user_id).subscribe(carts_data => {
+        const all_carts = carts_data['data'];
+        // Now find the cart instance with the selected product
+        for (let i = 0; i < all_carts.length; i++) {
+          const cart = (all_carts[i] as Cart);
+          // If product found in cart, assign it to 'product_cart' object
+          if (cart.Product_id === this.product.id) {
+            this.product_cart = cart;
+            break;
+          }
+        }
+      }, error => {
+        this.toastr.showError(error);
+      });
+
+      // Load the alert related to this product and user
+      // Only load alert when product quantity is 0
+      if (this.product.Quantity <= 0) {
+        this.productService.getAlert(this.product.id, this.user_id).subscribe(alert_data => {
+          if (alert_data['data']) {
+            this.is_alert_active = true;
+          } else {
+            this.is_alert_active = false;
+          }
+        }, error => {
+          this.toastr.showError(error);
+        });
+      }
+    }, error => {
+      this.toastr.showError(error);
+    });
+  }
+
+  createAlert() {
+    this.productService.createAlert(this.product.id, this.user_id).subscribe(data => {
+      this.toastr.showSuccess('Alert created successfully');
+      this.is_alert_active = true;
+    }, error => {
+      this.toastr.showError(error);
+    });
+  }
+
+  addProductToCart() {
+    // Input Validation
+    if (this.product_cart.Quantity == null || this.product_cart.Quantity <= 0 || isNaN(this.product_cart.Quantity)) {
+      this.toastr.showWarning('Invalid Quantity.');
+      return;
+    }
+    // Add to cart call
+    this.cartService.addProductToCart(this.product_cart).subscribe(data => {
+      this.toastr.showSuccess('Product added successfully');
+      this.product_cart = data['data'];
+    }, error => {
+      this.toastr.showError(error);
+    });
+  }
+
+  updateProductQtyInCart() {
+    // Input Validation
+    if (this.product_cart.Quantity == null || this.product_cart.Quantity < 0 || isNaN(this.product_cart.Quantity)) {
+      this.toastr.showWarning('Invalid Quantity.');
+      return;
+    }
+    // Update product quantity in cart call
+    this.cartService.changeProductQuantity(this.product_cart).subscribe(data => {
+      this.toastr.showSuccess(data['message']);
+      if (this.product_cart.Quantity === 0) {
+        // Meaning the cart is deleted from db.. Thus delete from client as well
+        this.product_cart.id = 0;
+      }
+    }, error => {
+      this.toastr.showError(error);
+    });
+  }
+
+  ngOnDestroy() {
+    // sub will only be initialized when url_part is fetched i.e. when user is not logged in
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 
 }
