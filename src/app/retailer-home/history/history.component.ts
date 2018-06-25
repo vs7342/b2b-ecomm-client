ae4f28b69be18f957d05d14c067c4abe5398d8e3
Vidit Singhal <vs7342@g.rit.edu>
2018-06-25 01:50:04 -0400

    Adding order history page UI + Logic. (Viewing order details modal will be added when ready)

--- src/app/retailer-home/history/history.component.ts
+++ src/app/retailer-home/history/history.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Order } from '../../models/Order.model';
+import { ToasterComponent } from '../../shared/toaster.component';
+import { ConstantsService } from '../../services/constants.service';
+import { OrderService } from '../../services/order.service';
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
-  styleUrls: ['./history.component.css']
+  styleUrls: ['./history.component.css'],
+  providers: [OrderService]
 })
 export class HistoryComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(ToasterComponent) toastr: ToasterComponent;
+  orders: Order[] = [];
+  user_id = 0;
 
-  ngOnInit() {
+  constructor(
+    private orderService: OrderService
+  ) { }
+
+  getOrderTotal(OrderProducts: any) {
+    let grand_total = 0;
+    // Loop over all the order products and calculate the total
+    OrderProducts.forEach(single_order_product => {
+      grand_total += single_order_product['Quantity'] * single_order_product['Product']['Price'];
+    });
+    // Return the total
+    return grand_total;
   }
 
+  ngOnInit() {
+    // Fetch user id
+    const user_id = ConstantsService.getLoggedInUserId();
+    if (user_id) {
+      // Save the user id and load orders
+      this.user_id = user_id;
+      this.loadOrders();
+    }
+  }
+
+  loadOrders() {
+    this.orderService.getAllOrdersForUser(this.user_id).subscribe(data => {
+      this.orders = data['data'];
+    }, error => {
+      this.toastr.showError(error);
+    });
+  }
+
+  viewOrder(order_id: number) {
+
+  }
 }
